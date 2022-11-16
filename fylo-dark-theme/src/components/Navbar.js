fa220d2fa5edd58d00af2fb46d55a86a70f8e2b6
oxTunde <phamus1111@gmail.com>
2022-11-16 21:37:26 +0100

    navbar done

---
+++ fylo-dark-theme/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import Logo from '../images/logo.svg'
+// import Profile from '../images/profile-3.jpg'
+
+function Navbar() {
+  return (
+    <nav>
+      <ul className="text-white float-right space-x-16 flex mt-12 mr-12">
+        <li className="absolute left-14"><img src={Logo} alt="Website-Logo"/></li>
+        <li className='mt-2'><a href="https://www.google.com">Features</a></li>
+        <li className='mt-2'><a href="https://www.google.com">Team</a></li>
+        <li className='mt-2'><a href="https://www.google.com">Sign in</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
