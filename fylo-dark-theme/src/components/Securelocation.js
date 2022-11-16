5dbe688f4dc4a7ee5e95f2ce6244e95a71a8e474
oxTunde <phamus1111@gmail.com>
2022-11-17 00:04:29 +0100

    securelocation modified

--- fylo-dark-theme/src/components/Securelocation.js
+++ fylo-dark-theme/src/components/Securelocation.js
@@ -5,7 +5,7 @@ import Securelocationtext from './Securelocationtext'
 function Securelocation(){
     return(
         <div className='Secure-location-text text-white'>
-            <Securelocationtext text={"All your files in one secure location,\n accessible anywhere"}/>
+            <Securelocationtext text={"All your files in one secure location,\n accessible anywhere."}/>
         </div>
     )
 }
