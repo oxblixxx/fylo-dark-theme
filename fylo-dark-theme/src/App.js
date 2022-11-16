4ced8ac5e515877a28eee2756c2101fe5fa482ac
oxTunde <phamus1111@gmail.com>
2022-11-16 21:47:41 +0100

    created navimage

--- fylo-dark-theme/src/App.js
+++ fylo-dark-theme/src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Navimage from "./components/Navimage"
 
 function App() {
   return (
     <div className='container'>
       <Navbar />
+      <Navimage />
     </div>
   )
 }
---
+++ fylo-dark-theme/src/components/Navimage.js
@@ -0,0 +1,10 @@
+import Introimage from '../images/illustration-intro.png'
+
+
+function Navimage() {
+    return(
+        <img className='center' src={Introimage} alt="" />
+    )
+}
+
+export default Navimage
