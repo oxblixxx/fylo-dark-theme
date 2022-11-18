cffcb6593578fae9941d17013169f23c5c468a4b
oxblixxx <phamust1111@gmail.com>
2022-11-18 09:31:13 +0100

    modified securelodation css

--- fylo-dark-theme/src/components/Securelocation.js
+++ fylo-dark-theme/src/components/Securelocation.js
@@ -4,7 +4,7 @@ function Securelocation(){
     return(
         <section className='secureLocationDesc text-white'>
             <p className="secureLocationText"> All your files in one secure location,</p>
-            <p className="accessibleText">accessible anywhere</p>
+            <p className="accessibleText">accessible anywhere.</p>
         </section>
     )
 }
