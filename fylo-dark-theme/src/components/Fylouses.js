f3db4731b376db4a36f1473164f2ec4eb876d1f7
oxblixxx <phamust1111@gmail.com>
2022-11-18 23:56:54 +0100

    created Fylousesdesc dot js file

--- fylo-dark-theme/src/components/Fylouses.js
+++ fylo-dark-theme/src/components/Fylouses.js
@@ -5,12 +5,12 @@ import './Fylouses.css'
 function Fylouses(){
     return(
         <nav className='fylo-uses p-64 text-white'>
-            <img className="p-4 ml-14" src={Iconfiles} alt="" />
-            <h1 className='ml-5'>Access your files, anywhere</h1>
+            <img className="p-4 ml-24" src={Iconfiles} alt="" />
+            <h1 className='ml-14'>Access your files, anywhere</h1>
             <p className='fylo-uses-text'>The ability to use a smartphone, tablet or computer</p>
             <p className='fylo-uses-text ml-2'>to acess your account means your files follow you
             </p> 
-            <p className="fylo-uses-text">everywhere. </p>
+            <p className="fylo-uses-text ml-28">everywhere. </p>
         </nav>
     )
 }
---
+++ fylo-dark-theme/src/components/Fylousesdesc.js
@@ -0,0 +1,31 @@
+const Fylousesdesc = [
+    {
+        id : 1,
+        header: "Access your files anywhere",
+        descriptionOne: "The ability to use a smartphone, a tablet or computer",
+        descriptionTwo: "to access your account means your files follow you",
+        descriptionThree: "everywhere",
+
+
+        id: 2,
+        header: "Security you can trust",
+        descriptionOne: "2-factor authenticationn and user-controlled encryption are",
+        descriptionTwo: "just a couple of the security features we allow to help",
+        descriptionThree: "secure your files",
+
+
+        id: 3,
+        header: "Real-time collaboration",
+        descriptionOne: "Securely share files and folders with friends, familt and",
+        descriptionTwo: "colleagus for the collaboration. No email attachments",
+        descriptionThree: "required",
+
+
+        id: 4,
+        header: "Store any type of file",
+        descriptionOne: "Whether you're sharing holidays photos or work",
+        descriptionTwo: "documents Fylo has you covered allowing for all the",
+        descriptionThree: "types to be scurel stored and shared."
+
+    }
+]
